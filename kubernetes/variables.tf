75be03e6765714784fc5d5ad9c34419e58fc14e2
Administrator <gitlab@mxmx.email>
2025-11-24 13:15:35 +0000

    Edit variables.tf

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,20 +1,3 @@
-variable "k8s_host" {
-  description = "Kubernetes API server URL"
-  type        = string
-}
-
-variable "k8s_token" {
-  description = "Service account token"
-  type        = string
-  sensitive   = true
-}
-
-variable "k8s_ca_cert" {
-  description = "Cluster CA certificate (base64 encoded)"
-  type        = string
-  sensitive   = true
-}
-
 variable "pihole_password" {
   description = "Pi-hole web admin password"
   type        = string
