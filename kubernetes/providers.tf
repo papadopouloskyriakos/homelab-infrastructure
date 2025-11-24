d55b2174c72c27e29989ac3e000eda25d0c69eea
llzzrrdd <ncpjfuzl@mxmx.email>
2025-11-24 18:43:07 +0100

    Migrate K8s workloads to OpenTofu

---
+++ kubernetes/providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "REDACTED_1158da07"
+      version = "~> 2.35"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+  }
+
+  backend "http" {
+    # Backend config provided via environment variables in CI:
+    # TF_HTTP_ADDRESS, TF_HTTP_LOCK_ADDRESS, TF_HTTP_UNLOCK_ADDRESS
+    # TF_HTTP_USERNAME, TF_HTTP_PASSWORD
+    # TF_HTTP_LOCK_METHOD, TF_HTTP_UNLOCK_METHOD
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.k8s_host
+  token                  = var.k8s_token
+  cluster_ca_certificate = base64decode(var.k8s_ca_cert)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.k8s_host
+    token                  = var.k8s_token
+    cluster_ca_certificate = base64decode(var.k8s_ca_cert)
+  }
+}
