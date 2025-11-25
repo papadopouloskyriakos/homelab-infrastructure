18fab6922b87fb842506cc4e9759ec9e9d737f25
Administrator <gitlab@mxmx.email>
2025-11-25 17:11:49 +0000

    Edit outputs.tf

--- kubernetes/namespaces/monitoring/outputs.tf
+++ kubernetes/namespaces/monitoring/outputs.tf
@@ -1,7 +1,23 @@
+***REMOVED***
+# Outputs for Monitoring Module
+***REMOVED***
+
 output "namespace" {
-  value = "monitoring"
+  description = "Namespace where monitoring is deployed"
+  value       = helm_release.monitoring.metadata[0].namespace
 }
 
-output "helm_release" {
-  value = helm_release.monitoring.metadata[0].name
+output "chart_version" {
+  description = "Deployed Helm chart version"
+  value       = helm_release.monitoring.metadata[0].version
+}
+
+output "grafana_nodeport" {
+  description = "Grafana NodePort"
+  value       = 30000
+}
+
+output "prometheus_nodeport" {
+  description = "Prometheus NodePort"
+  value       = 30090
 }
