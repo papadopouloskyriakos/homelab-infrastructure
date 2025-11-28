1bbd644e4cc64a1193f55d468b85693940cd5dd9
llzzrrdd <ncpjfuzl@mxmx.email>
2025-11-29 00:55:15 +0100

    feat(pihole): enable mTLS authentication for web UI and Prometheus

--- kubernetes/namespaces/pihole/network-policies.tf
+++ kubernetes/namespaces/pihole/network-policies.tf
@@ -62,7 +62,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           REDACTED_6fa691d2 = {
-            mode = "disabled"
+            mode = "required"
           }
         },
         {
@@ -82,7 +82,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           REDACTED_6fa691d2 = {
-            mode = "disabled"
+            mode = "required"
           }
         }
       ]
