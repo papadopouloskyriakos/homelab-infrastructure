279696b36ed38531c4ec76c229f736e3251e2940
llzzrrdd <ncpjfuzl@mxmx.email>
2025-11-28 22:46:52 +0100

    fix(pihole): use valid mTLS mode (disabled)

--- kubernetes/namespaces/pihole/network-policies.tf
+++ kubernetes/namespaces/pihole/network-policies.tf
@@ -62,7 +62,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           REDACTED_6fa691d2 = {
-            mode = "optional"
+            mode = "disabled"
           }
         },
         {
@@ -82,7 +82,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           REDACTED_6fa691d2 = {
-            mode = "optional"
+            mode = "disabled"
           }
         }
       ]
