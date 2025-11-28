72a79d00cfc5a99cbe2333cbe9d9658a1143316d
llzzrrdd <ncpjfuzl@mxmx.email>
2025-11-28 22:43:37 +0100

    fix(pihole): change mTLS to optional mode

--- kubernetes/namespaces/pihole/network-policies.tf
+++ kubernetes/namespaces/pihole/network-policies.tf
@@ -62,7 +62,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           authentication = {
-            mode = "required"
+            mode = "optional"
           }
         },
         {
@@ -82,7 +82,7 @@ resource "kubernetes_manifest" "pihole_network_policy" {
             }
           ]
           authentication = {
-            mode = "required"
+            mode = "optional"
           }
         }
       ]
