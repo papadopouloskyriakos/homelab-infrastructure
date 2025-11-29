e8e08422b398175474dac93898ab144306b8d62b
llzzrrdd <ncpjfuzl@mxmx.email>
2025-11-29 03:12:50 +0100

    fix: update helm provider and resources to v3 syntax

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -10,7 +10,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = var.k8s_host
     token                  = var.k8s_token
     cluster_ca_certificate = base64decode(var.k8s_ca_cert)
