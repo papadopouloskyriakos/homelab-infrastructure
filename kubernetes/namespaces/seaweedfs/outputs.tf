350f52e7b06bf9ae92e207ffe6cd6bd5d40f8f04
llzzrrdd <ncpjfuzl@mxmx.email>
2025-12-10 22:03:53 +0100

    feat(seaweedfs): add SeaweedFS distributed object storage
    - 3 master replicas (Raft consensus)
    - 2 volume servers (500Gi each = 1TB total)
    - 2 filer servers (S3 API gateway)
    - Node affinity to local region (nl-lei)
    - Anti-affinity to spread across nodes
    - ESO integration for S3 credentials
    - ServiceMonitor for Prometheus

---
+++ kubernetes/namespaces/seaweedfs/outputs.tf
@@ -0,0 +1,23 @@
+***REMOVED***
+# SeaweedFS Outputs
+***REMOVED***
+
+output "namespace" {
+  description = "SeaweedFS namespace"
+  value       = kubernetes_namespace.seaweedfs.metadata[0].name
+}
+
+output "s3_endpoint" {
+  description = "S3-compatible API endpoint (internal)"
+  value       = "http://seaweedfs-filer.seaweedfs.svc.cluster.local:8333"
+}
+
+output "filer_endpoint" {
+  description = "Filer HTTP endpoint (internal)"
+  value       = "http://seaweedfs-filer.seaweedfs.svc.cluster.local:8888"
+}
+
+output "master_endpoint" {
+  description = "Master HTTP endpoint (internal)"
+  value       = "http://seaweedfs-master.seaweedfs.svc.cluster.local:9333"
+}
