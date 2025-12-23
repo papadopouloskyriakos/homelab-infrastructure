7df0c70bfcd10db876df148d6871197d89ffbb12
llzzrrdd <ncpjfuzl@mxmx.email>
2025-12-23 21:23:15 +0100

    fix(gatus): Hardcode HAProxy auth temporarily for testing

--- kubernetes/namespaces/gatus/main.tf
+++ kubernetes/namespaces/gatus/main.tf
@@ -170,7 +170,7 @@ resource "REDACTED_a9df2e77_v1" "gatus_config" {
             url      = "http://chzrh01vps01-int.example.net:8404/stats;csv"
             interval = "60s"
             headers = {
-              Authorization = "Basic ${var.haproxy_stats_auth}"
+	      Authorization = "Basic REDACTED_38a2053f"
             }
             conditions = [
               "[STATUS] == 200"
@@ -183,7 +183,7 @@ resource "REDACTED_a9df2e77_v1" "gatus_config" {
             url      = "http://notrf01vps01-int.example.net:8404/stats;csv"
             interval = "60s"
             headers = {
-              Authorization = "Basic ${var.haproxy_stats_auth}"
+	      Authorization = "Basic REDACTED_38a2053f"
             }
             conditions = [
               "[STATUS] == 200"
