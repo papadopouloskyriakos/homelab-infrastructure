e00fd06da1e5c466f77176782e860bb4e3807f29
llzzrrdd <ncpjfuzl@mxmx.email>
2025-12-23 05:39:43 +0100

    fix(gatus): pass alerting token from parent module

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -214,8 +214,8 @@ module "REDACTED_ac4dcdf5" {
 ***REMOVED***
 module "gatus" {
   source = "./namespaces/gatus"
-
   depends_on = [module.ingress_nginx, module.cert_manager]
+  REDACTED_4f32e8a8 = var.REDACTED_2636fc38
 }
 ***REMOVED***
 # Well-Known Endpoints (security.txt, etc.)
